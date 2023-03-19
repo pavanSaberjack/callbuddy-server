aba6c5d7283aef4a821392764d3d1e6c9d6f552c
Pavan Itagi <pavan.itagi@ymedialabs.com>
2023-03-19 07:51:21 +0530

    Creating a sample function to run at time intervals

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ app.use(eventRouter);
 app.use(userRouter);
 
 app.listen(8080);
+
+function myFunction() {
+    console.log("This function runs every 5 minutes.");
+}
+
+const intervalId = setInterval(myFunction, 0.5 * 60 * 1000); // run every 30 seconds (min * 60 seconds * 1000 milliseconds)
+  
+// stop the interval after 2 minutes (4 iterations)
+setTimeout(() => {
+    clearInterval(intervalId);
+}, 2 * 60 * 1000); 
