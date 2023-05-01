9e8761e9159bbf0028a6b637923a7d95870b28c3
Pavan <itagi75@gmail.com>
2023-05-01 17:22:27 +0530

    Changed the user table to accept idtoken

--- models/user.js
+++ models/user.js
@@ -6,13 +6,18 @@ module.exports = class User {
         this.googleAuthJSON = googleAuthJSON
     }
 
-    static update(emailId, googleAuthJSON) {
-        const query = `UPDATE user SET googleAuthJSON="${googleAuthJSON}" WHERE emailId="${emailId}"`;
+    static update(emailId, idToken) {
+        const query = `UPDATE user SET idToken="${idToken}" WHERE emailId="${emailId}"`;
         return db.execute(query);
     }
 
-    static create(emailId, googleAuthJSON) {
-        const query = `INSERT INTO user (emailId, googleAuthJSON) VALUES ("${emailId}", "${googleAuthJSON}")`;
+    static update(emailId, accessToken, refreshToken) {
+        const query = `UPDATE user SET accessToken="${accessToken}, refreshToken="${refreshToken}" WHERE emailId="${emailId}"`;
+        return db.execute(query);
+    }
+
+    static create(emailId, idToken) {
+        const query = `INSERT INTO user (emailId, idToken) VALUES ("${emailId}", "${idToken}")`;
         return db.execute(query);
     }
 
